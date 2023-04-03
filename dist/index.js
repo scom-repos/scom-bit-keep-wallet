685c43f5de3bb63e8a40b8cdc3367f0a4f3fc756
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2023-04-03 19:20:50 +0800

    first commit

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+define("@scom/scom-bit-keep-wallet", ["require", "exports", "@ijstech/eth-wallet", "@ijstech/components"], function (require, exports, eth_wallet_1, components_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.BitKeepWalletProvider = void 0;
+    let moduleDir = components_1.application.currentModuleDir;
+    function fullPath(path) {
+        if (path.indexOf('://') > 0)
+            return path;
+        return `${moduleDir}/${path}`;
+    }
+    class BitKeepWalletProvider extends eth_wallet_1.EthereumProvider {
+        get displayName() {
+            return 'BitKeep Wallet';
+        }
+        get image() {
+            return fullPath('img/BitKeep.png');
+        }
+        get provider() {
+            return window['bitkeep']['ethereum'];
+        }
+        get homepage() {
+            return 'https://bitkeep.com/download?type=2';
+        }
+        installed() {
+            return !!window['isBitKeep'];
+        }
+    }
+    exports.BitKeepWalletProvider = BitKeepWalletProvider;
+});
---
+++ pluginTypes/index.d.ts
@@ -0,0 +1,11 @@
+/// <amd-module name="@scom/scom-bit-keep-wallet" />
+declare module "@scom/scom-bit-keep-wallet" {
+    import { EthereumProvider } from '@ijstech/eth-wallet';
+    export class BitKeepWalletProvider extends EthereumProvider {
+        get displayName(): string;
+        get image(): string;
+        get provider(): any;
+        get homepage(): string;
+        installed(): boolean;
+    }
+}
